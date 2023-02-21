a3d398862acb63caa773db645d831911e174f5a1
solvd <solvd@solvds-MacBook-Pro-2.local>
2023-02-21 11:23:39 +0300

    Tests were changed

--- src/main/java/com/solvd/posteducationtaskscarina/component/LocationElement.java
+++ src/main/java/com/solvd/posteducationtaskscarina/component/LocationElement.java
@@ -3,8 +3,11 @@ package com.solvd.posteducationtaskscarina.component;
 import com.qaprosoft.carina.core.foundation.webdriver.decorator.ExtendedWebElement;
 import com.qaprosoft.carina.core.gui.AbstractUIObject;
 import com.zebrunner.carina.utils.mobile.IMobileUtils;
+import org.openqa.selenium.By;
 import org.openqa.selenium.SearchContext;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.FindBy;
 
 public class LocationElement extends AbstractUIObject implements IMobileUtils {
@@ -14,9 +17,6 @@ public class LocationElement extends AbstractUIObject implements IMobileUtils {
 
     @FindBy(css = ".a-popover-wrapper")
     private ExtendedWebElement chooseYourLocationWindow;
-
-    @FindBy(xpath = "//*[contains(@class, 'a-icon') and contains(@class, 'a-icon-dropdown')]")
-    private ExtendedWebElement location;
 
     @FindBy(id = "GLUXCountryList_178")
     private ExtendedWebElement country;
@@ -32,10 +32,15 @@ public class LocationElement extends AbstractUIObject implements IMobileUtils {
         locationButton.click();
 //        ExtendedWebElement chooseYourLocationWindow = getChooseYourLocationWindow();
 //        if (chooseYourLocationWindow.isElementPresent()) {
-            location.click();
-            country.scrollTo();
-            country.click();
-            doneButton.click();
+
+        WebElement location = driver
+                .findElement(By.xpath("//*[contains(@class, 'a-icon') and contains(@class, 'a-icon-dropdown')]"));
+        Actions actions = new Actions(driver);
+        actions.moveToElement(location).click().build().perform();
+        location.click();
+        country.scrollTo();
+        country.click();
+        doneButton.click();
 //        }
     }
 
