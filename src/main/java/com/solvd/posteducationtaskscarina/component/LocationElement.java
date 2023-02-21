e5450816bc651ae95714f2ed164573cbaf6d8d24
solvd <solvd@solvds-MacBook-Pro-2.local>
2023-02-21 11:31:14 +0300

    Tests were changed

--- src/main/java/com/solvd/posteducationtaskscarina/component/LocationElement.java
+++ src/main/java/com/solvd/posteducationtaskscarina/component/LocationElement.java
@@ -37,7 +37,6 @@ public class LocationElement extends AbstractUIObject implements IMobileUtils {
                 .findElement(By.xpath("//*[contains(@class, 'a-icon') and contains(@class, 'a-icon-dropdown')]"));
         Actions actions = new Actions(driver);
         actions.moveToElement(location).click().build().perform();
-        location.click();
         country.scrollTo();
         country.click();
         doneButton.click();
