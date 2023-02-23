4e1065b475ab0db04b73731d4b23db5411518398
solvd <solvd@solvds-MacBook-Pro-2.local>
2023-02-23 17:07:32 +0300

    Tests were updated

--- src/main/java/com/solvd/posteducationtaskscarina/component/LocationElement.java
+++ src/main/java/com/solvd/posteducationtaskscarina/component/LocationElement.java
@@ -2,37 +2,20 @@ package com.solvd.posteducationtaskscarina.component;
 
 import com.qaprosoft.carina.core.foundation.webdriver.decorator.ExtendedWebElement;
 import com.qaprosoft.carina.core.gui.AbstractUIObject;
-import com.zebrunner.carina.utils.mobile.IMobileUtils;
 import org.openqa.selenium.SearchContext;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.support.FindBy;
 
-public class LocationElement extends AbstractUIObject implements IMobileUtils {
+public class LocationElement extends AbstractUIObject {
 
-    @FindBy(id = "nav-packard-glow-loc-icon")
-    private ExtendedWebElement locationButton;
-
-    @FindBy(css = ".a-popover-wrapper")
-    private ExtendedWebElement chooseYourLocationWindow;
-
-    @FindBy(xpath = "//*[@data-action='a-dropdown-button']")
-    private ExtendedWebElement location;
-
-    @FindBy(id = "GLUXCountryList_178")
-    private ExtendedWebElement country;
-
-    @FindBy(xpath = "//*[@name='glowDoneButton']")
-    private ExtendedWebElement doneButton;
+    @FindBy(xpath = ".//*[@data-action-type='DISMISS']")
+    private ExtendedWebElement dismissButton;
 
     public LocationElement(WebDriver driver, SearchContext searchContext) {
         super(driver, searchContext);
     }
 
-    public void chooseLocation() {
-        locationButton.click();
-        location.clickByJs();
-        country.scrollTo();
-        country.clickByJs();
-        doneButton.clickByJs();
+    public void clickDismissButton() {
+        dismissButton.click();
     }
 }
